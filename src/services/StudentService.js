1c52eaba6280136e8152bb34f256ae0a738b8aed
Harish_Kavinda <120238769+harishkavindauoj@users.noreply.github.com>
2023-03-08 22:20:11 +0530

    update

--- src/services/StudentService.js
+++ src/services/StudentService.js
@@ -2,23 +2,19 @@ import axios from 'axios';
 
 const STUDENT_API_BASE_URL = 'http://localhost:8080/api/v1/students';
 
-
-class StudentService{
-
-    getStudents(){
+class StudentService {
+    getStudents() {
         return axios.get(STUDENT_API_BASE_URL);
     }
 
-    addStudent(student){
+    addStudent(student) {
         return axios.post(STUDENT_API_BASE_URL, student);
     }
 
     deleteStudent(studentId) {
         return axios.delete(STUDENT_API_BASE_URL + '/' + studentId);
     }
-
 }
 
-  
-  export default new StudentService();
 
+export default new StudentService();
